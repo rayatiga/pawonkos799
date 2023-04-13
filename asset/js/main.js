e0e98db522b4ee9d14a775ca622194001febe5b8
bydzen <svnteentthones@gmail.com>
2023-04-13 15:35:40 +0700

    add uri and product list

--- asset/js/main.js
+++ asset/js/main.js
@@ -17,3 +17,26 @@ if (scrollTopButton) {
         document.documentElement.scrollTop = 0
     }
 }
+var uri = document.getElementById('uri')
+if (uri) {
+    uri.innerHTML = window.location.href
+}
+var productList = document.querySelector('.responsiveProductList1')
+if (productList) {
+    var productList1 = document.querySelector('.responsiveProductList1')
+    var productList2 = document.querySelector('.responsiveProductList2')
+
+    window.onresize = function () {
+        if (window.innerWidth <= 352) {
+            productList1.classList.add('row-cols-1')
+            productList1.classList.remove('row-cols-2')
+            productList2.classList.add('row-cols-1')
+            productList2.classList.remove('row-cols-2')
+        } else {
+            productList1.classList.add('row-cols-2')
+            productList1.classList.remove('row-cols-1')
+            productList2.classList.add('row-cols-2')
+            productList2.classList.remove('row-cols-1')
+        }
+    }
+}
